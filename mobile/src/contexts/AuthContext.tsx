e5e511ef2ff76daaeefb0e7dafff2d7ae17a9171
ErickSilva2605 <erick.dev.it@gmail.com>
2023-02-17 00:11:28 -0300

    Implementando autenticação com google

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,4 +1,9 @@
-import { createContext, ReactNode } from 'react';
+import { createContext, ReactNode, useState, useEffect } from 'react';
+import * as Google from 'expo-auth-session/providers/google';
+import * as AuthSession from 'expo-auth-session';
+import * as WebBrowser from 'expo-web-browser';
+
+WebBrowser.maybeCompleteAuthSession();
 
 interface IUserProps {
   name: string;
@@ -7,6 +12,7 @@ interface IUserProps {
 
 export interface IAuthContextProps {
   user: IUserProps;
+  isUserLoading: boolean;
   signIn: () => Promise<void>;
 }
 
@@ -17,18 +23,43 @@ interface IAuthProvider {
 export const AuthContext = createContext({} as IAuthContextProps);
 
 export function AuthContextProvider({ children }: IAuthProvider) {
+  const [user, setUser] = useState<IUserProps>({} as IUserProps);
+  const [isUserLoading, setIsUserLoading] = useState(false);
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    clientId: '',
+    redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
+    scopes: ['profile', 'email']
+  });
 
   async function signIn() {
+    try {
+      setIsUserLoading(true);
 
+      await promptAsync();
+    } catch (error) {
+      console.error(error);
+      throw error;
+    } finally {
+      setIsUserLoading(false);
+    }
   }
+
+  async function signInWithGoogle(access_token: string) {
+    console.log("TOKEN ==>", access_token);
+  }
+
+  useEffect(() => {
+    if (response?.type === 'success' && response.authentication?.accessToken) {
+      signInWithGoogle(response.authentication.accessToken);
+    }
+  }, [response]);
 
   return (
     <AuthContext.Provider value={{
+      isUserLoading,
       signIn,
-      user: {
-        name: 'Erick Augusto',
-        avatarUrl: 'https://github.com/ErickSilva2605.png'
-      }
+      user
     }}>
       {children}
     </AuthContext.Provider>
--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,11 +1,14 @@
 import { Center, Text, Icon } from 'native-base';
 import { Fontisto } from '@expo/vector-icons';
 
+import { useAuth } from '../hooks/useAuth';
 import { Button } from '../components/Button';
 
 import Logo from '../assets/logo.svg';
 
 export function SignIn() {
+  const { signIn } = useAuth();
+
   return (
     <Center flex={1} bgColor="gray.900" p={7}>
       <Logo width={212} height={40} />
@@ -18,6 +21,7 @@ export function SignIn() {
         _loading={{
           _spinner: { color: 'white' }
         }}
+        onPress={signIn}
       />
 
       <Text color="white" textAlign="center" mt={4}>
