701301a1ba4844ad56cc68879b6804743817cf48
ErickSilva2605 <erick.dev.it@gmail.com>
2023-02-21 22:51:37 -0300

    Obtendo os bolões da api

--- mobile/src/screens/Pools.tsx
+++ mobile/src/screens/Pools.tsx
@@ -1,12 +1,43 @@
-import { VStack, Icon } from 'native-base';
-import { useNavigation } from '@react-navigation/native';
+import { useCallback, useState } from 'react';
+import { VStack, Icon, useToast, FlatList } from 'native-base';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
 import { Octicons } from '@expo/vector-icons';
+
+import { api } from '../services/api';
 
 import { Button } from '../components/Button';
 import { Header } from '../components/Header';
+import { PoolCard, PoolCardProps } from '../components/PoolCard';
+import { Loading } from '../components/Loading';
+import { EmptyPoolList } from '../components/EmptyPoolList';
 
 export function Pools() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [pools, setPools] = useState<PoolCardProps[]>([])
+
   const { navigate } = useNavigation();
+  const toast = useToast();
+
+  async function fetchPools() {
+    try {
+      setIsLoading(true);
+      const response = await api.get('/pools');
+      setPools(response.data.pools);
+    } catch (error) {
+      console.error(error);
+      toast.show({
+        title: 'Não foi possível carregar os bolões',
+        placement: 'top',
+        bgColor: 'red.500'
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useFocusEffect(useCallback(() => {
+    fetchPools();
+  }, []));
 
   return (
     <VStack flex={1} bg="gray.900">
@@ -19,6 +50,23 @@ export function Pools() {
           onPress={() => navigate('find')}
         />
       </VStack>
+
+      {
+        isLoading ? <Loading /> :
+          <FlatList
+            data={pools}
+            keyExtractor={item => item.id}
+            renderItem={({ item }) => (
+              <PoolCard
+                data={item}
+              />
+            )}
+            ListEmptyComponent={<EmptyPoolList />}
+            showsVerticalScrollIndicator={false}
+            _contentContainerStyle={{ pb: 10 }}
+            px={5}
+          />
+      }
     </VStack>
   );
 }
